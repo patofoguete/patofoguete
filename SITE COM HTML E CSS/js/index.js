f6d47a3ece09f420e9a8328cbb1091854b210edc
patofoguete <126728781+patofoguete@users.noreply.github.com>
2023-03-01 19:57:39 -0300

    Add files via upload

---
+++ SITE COM HTML E CSS/js/index.js
@@ -0,0 +1,23 @@
+function CriarConta(){
+    window.location = "CriarConta.html";
+}
+
+function foguetes(){
+    window.location ="foguetes.html"
+}
+
+function home(){
+    window.location = "intex.html";
+}
+
+function sobre(){
+    window.location ="sobre.html"
+}
+
+function equipe(){
+    window.location ="equipe.html"
+}
+
+function entrar(){
+    window.location ="entrar.html"
+}
